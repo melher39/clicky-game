42634b2d624db58d9b4e33470474e9ba8d95c9ad
Melvin Hernandez <melher39@gmail.com>
2019-03-08 12:55:23 -0600

    added unique artist images and added a style.css to project

--- clicky-game/src/components/ArtistImage.js
+++ clicky-game/src/components/ArtistImage.js
@@ -3,7 +3,7 @@ import React from "react";
 function ArtistImage(props){
     return(
         <div className="col s6 m3">
-            <img className="responsive-img circle" src={props.photo.url} alt={props.photo.name}></img>
+            <img className="circle" src={props.photo.url} alt={props.photo.name}></img>
             
         </div>
     );
